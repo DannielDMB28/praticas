f96fffcbcc22168ed6af3cb186c3eeb600223261
DannielDMB28 <danniel.moreiira04@hotmail.com>
2025-08-26 19:12:57 -0300

    minis projetos

---
+++ mini-projetos-simples/media-de-notas/index.js
@@ -0,0 +1,13 @@
+let nota1 = Number(prompt('Digite sua primeira nota: '));
+let nota2 = Number(prompt('Digite sua segunda nota: '));
+let nota3 = Number(prompt('Digite sua terceira nota: '));
+let media = (nota1 + nota2 + nota3) / 2;
+alert(`Sua media é: ${media}`);
+
+let = media;
+
+if (media >= 7) {
+    alert(`Parabens esta aprovado!`);
+}  else {
+    alert(`Reprovado!`);
+}
---
+++ mini-projetos-simples/sorteado-de-numeros/index.js
@@ -0,0 +1,3 @@
+let gerador = Math.floor(Math.random() * 100) + 1;
+
+console.log(`O numero sorteado foi: ${gerador}`);
